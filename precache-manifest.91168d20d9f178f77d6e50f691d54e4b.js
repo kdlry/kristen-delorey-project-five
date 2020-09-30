b45926bbf9d1826633d95bbcb249dc03d9c7ffd1
kdlry <kristen.delorey@live.com>
2020-09-30 11:58:25 -0400

    Updates

---
+++ precache-manifest.91168d20d9f178f77d6e50f691d54e4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec18e969cf46b4af1a3ed7e783a6f677",
+    "url": "/kristen-delorey-project-five/index.html"
+  },
+  {
+    "revision": "a0f332838810ff6ecf28",
+    "url": "/kristen-delorey-project-five/static/css/main.c085fca2.chunk.css"
+  },
+  {
+    "revision": "153e5f1c6d3709b65cba",
+    "url": "/kristen-delorey-project-five/static/js/2.588d4dc2.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/kristen-delorey-project-five/static/js/2.588d4dc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f332838810ff6ecf28",
+    "url": "/kristen-delorey-project-five/static/js/main.28a5eecb.chunk.js"
+  },
+  {
+    "revision": "18ae56dab328b966075b",
+    "url": "/kristen-delorey-project-five/static/js/runtime-main.b7903612.js"
+  },
+  {
+    "revision": "a02d0a1794ab516ec860f2997acc3bc4",
+    "url": "/kristen-delorey-project-five/static/media/alba.a02d0a17.TTF"
+  },
+  {
+    "revision": "b40a88ee48d4e3d4573f334de719484c",
+    "url": "/kristen-delorey-project-five/static/media/marketDeco.b40a88ee.ttf"
+  }
+]);
